0efe1274a0e033381928939dcbd628c53547f980
i2ivision TeamLeader <ab@i2ivision.net>
2016-01-08 12:03:48 +0200

    A WordPress Plugin beta release

---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Search Plus Export Users ===
+Auther: WR-phpdev5
+Version: 1.0.0
+Plugin URL: www.i2ivision.com
+Tags: search,filter,export,csv,file,users,meta,keys,role,operation
+Requires at least: 3.0
+Tested up to: 4.3
+Stable tag: 4.3.1
+License: GPLv2
+
+Search Plus Export Users is a plugin for Searching for users with specific keywords and Exporting results in CSV file.
+
+=== Description ===
+Search Plus Export Users's Features:
+
+1. you can get all users in your site by clicking "Get All Users" button to return all users on the table with fields:{ ID - User Name - Email }.
+2. you can search for users with specifc keywords such as :
+	a. choose a "Role" that you want to search with.
+	b. choose an operation that you want for search result:
+		b1. if "AND", you should search with valid keyword for users because if you type a missing name in "search for value field" that hasn't relationship with key in drop-down menu,no data will return
+		b2. if "OR", you should search with valid keyword for users and if you type a missing name in "search for value field",it will return valid result
+		and if all search values are valid , it will return all result for all search values you typed.
+	c. after that, choose filed from drop-down menu that you want to search with and type search value in "search for value" field next to.
+3. you can search with multiple keys and values by clicking "+" button
+4. you can remove fields that you added for searching, if you don't need it.
+5. then click "Search" button to show results on the table.
+6. after searching for users that you want, you can click "Export" button to export results in CSV file.
+7. you can add "Notes" for users that used this plugin by clicking "Setting" menu in the plugin, add notes then it will show in "Notes" box in the plugin's page.
+
+
+=== Installation ===
+1.  Upload your plugin folder to the '/wp-content/plugins\' directory.
+2.  Activate the plugin through the 'Plugins\' menu in WordPress.
+
+
+
+=== Frequently Asked Questions ===
+there is no FAQ just yet.
+
+
+=== Screenshots ===
+There is no screenshots just yet.
+
+
+=== Changelog ===
+= 1.0.0 =
+First release
+
+=== Upgrade Notice ===
+There is no need to upgrade just yet.
---
+++ search_plus_export_users.php
@@ -0,0 +1,263 @@
+<?php
+
+/**
+ * @package Search plus Export Users
+ */
+/**
+ * Plugin Name: Search plus Export Users
+ * Plugin URI: http://www.i2ivision.com
+ * Description: Search (filter) for users based on specific keywords then export results in CSV file
+ * Author: i2ivision ( PHPdev5 )
+ * Version: 0.1.5 beta
+ * Author URI: http://www.i2ivision.com
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+?>
+
+<?php
+
+/**
+ * @author: PHPdev5
+ * [export_users_actions Add menu page [Export Users] for our plugin]
+ * @return [None]
+ */
+function export_users_actions() {
+    //add menu page for [Search plus Export Users] plugin
+    add_menu_page( "Export Users", "Export Users", 'manage_options', "export_users", "export_users_fn", "dashicons-format-aside", 26 );
+    
+    //add submenu page for setting page
+    add_submenu_page( 'export_users', 'Notes Setting', 'Notes', 'manage_options', 'notes', 'setting_fn' );
+}
+
+add_action( 'admin_menu', 'export_users_actions' );
+
+/**
+ * @author: PHPdev5
+ * [export_users_fn Build the Back-End of our plugin]
+ * @return [None]
+ */
+function export_users_fn() {
+    /**
+     * [$html_template filter hook to allow users for changing plugin's template page]
+     */
+    $html_template = apply_filters( 'export_users_table_template','export_users_admin.php' );
+    require_once( $html_template );
+}
+
+/**
+ * @author: PHPdev5
+ * [setting_fn Build the Setting page of our plugin]
+ * @return [None]
+ */
+function setting_fn() {
+    require_once( 'setting.php' );
+}
+
+/**
+ * @author: PHPdev5
+ * [plugin_helper_scripts Enqueue Scripts & Styles]
+ * @return [None]
+ */
+function plugin_helper_scripts() {
+    wp_enqueue_script( 'script_get_users', plugins_url( '/js/get_users.js', __FILE__ ) );
+    wp_enqueue_script( 'script_Block_UI', plugins_url( '/js/jquery.blockUI.js', __FILE__ ) );
+    wp_enqueue_style( 'script_get_users', plugins_url( '/css/styles.css', __FILE__ ) );
+    wp_localize_script( 'script_get_users', 'MyAjax', array( 'ajaxurl'   => admin_url( 'admin-ajax.php' ) ) );
+}
+
+add_action( 'admin_init', 'plugin_helper_scripts' );
+
+/**
+ * @author: PHPdev5
+ * [my_load_meta_key load all meta_key in drop down menu]
+ * @return [Array]
+ */
+function load_meta_key() {
+    check_ajax_referer( 'bk-ajax-nonce', 'security' );
+    global $wpdb;
+    $customkeys = array();
+    $customkeys[] = $wpdb->get_results( "SELECT DISTINCT meta_key FROM $wpdb->usermeta", ARRAY_A );
+    foreach ( $customkeys as $customvalue ) {
+        $customvalue;
+    }
+    wp_send_json( $customvalue );
+}
+
+add_action( 'wp_ajax_nopriv_load-meta', 'load_meta_key' );
+add_action( 'wp_ajax_load-meta', 'load_meta_key' );
+
+/**
+ * @author: PHPdev5
+ * [view_all_users Get all users on site]
+ * @return [Array]
+ */
+function view_all_users() {
+    check_ajax_referer( 'bk-ajax-nonce', 'security' );
+    $content = array();
+    $all_users = get_users();
+    foreach ( $all_users as $e_user ) {
+        $content[] = array( "userid" => $e_user->ID, "username" => $e_user->display_name, "useremail" => $e_user->user_email );
+    }
+    wp_send_json( $content );
+}
+
+add_action( 'wp_ajax_nopriv_get_all_users', 'view_all_users' );
+add_action( 'wp_ajax_get_all_users', 'view_all_users' );
+
+
+/**
+ * @author: PHPdev5
+ * [generate_csv Generate CSV file including Search Results]
+ * @return [None]
+ */
+function generate_csv() {
+    if ( isset( $_POST['download_csv'] ) ) {
+        $nonce = $_REQUEST['_wpnonce'];
+        if ( !wp_verify_nonce( $nonce, 'export-form' ) ) {
+            die( 'Security check' );
+        }
+        ob_end_clean();
+        $name = "export-results_" . date('Y-m-d') . ".csv";
+        header( 'Content-Type: text/csv; charset=utf-8' );
+        header( 'Content-Disposition: attachment; filename=' . $name );
+        $op = fopen( 'php://output', 'a' );
+        $en_active_export = append_all_meta( 1 );
+        $count = 0;
+        if ( !empty( $en_active_export ) ) {
+            if ( $_REQUEST["hidden_val"] == "D" ) {
+                $finalHeader = array( 'ID', 'User Name', 'E-mail' );
+            } else {
+                $meta_key = $_REQUEST["meta_name"];
+                $headerArray = array( 'ID', 'User Name', 'E-mail' );
+                $headermerge = array_merge( $headerArray, $meta_key );
+                $finalHeader = array_unique( $headermerge );
+            }
+            fputcsv( $op, $finalHeader );
+            foreach ( $en_active_export as $row ) {
+                fputcsv( $op, $row );
+                $count++;
+            }
+        } else {
+            fputcsv( $op, "" );
+        }
+        fclose( $op );
+        die();
+    }
+}
+
+add_action( "admin_init", 'generate_csv' );
+
+/**
+ * @author: PHPdev5
+ * @Description: Get users based on Specific Values  
+ * @return [Array]
+ */
+function append_all_meta( $check = 0 ) {
+    $operation = $_REQUEST["op_args"];
+    $meta_key = $_REQUEST["meta_name"];
+    $meta_value = $_REQUEST["meta_value"];
+    $meta_compare = $_REQUEST["meta_compare"];
+    $role_data = $_REQUEST["user_role"];
+    if ( $role_data == 'no_role' ) {
+        $role_data = '';
+    }
+    if ( $_REQUEST["hidden_val"] == "D" ) {
+        $content = array();
+        $all_users = get_users();
+        foreach ( $all_users as $e_user ) {
+            $content[] = array( $e_user->ID, $e_user->display_name, $e_user->user_email );
+        }
+        return $content;
+    }
+
+    // Allow Duplicate Keys or Values for Associative Array
+    function duplicateKeys( $key, $val ) {
+        return array( $key => $val );
+    }
+    $arrayResult = array_map( 'duplicateKeys', $meta_key, $meta_value );
+    //End
+
+    $queryArray = array( 'relation' => $operation );
+    $count = 0;
+    foreach ( $arrayResult as $key => $value ) {
+        foreach ( $value as $Metakey => $Metavalue ) {
+            $metaArray = array( "key"     => $Metakey,
+                                "value"   => $Metavalue,
+                                "compare" => $meta_compare[$count] );
+            array_push( $queryArray, $metaArray );
+            $count++;
+        }
+    }
+    $args = array(
+                   'role'       => $role_data,
+                   'meta_query' => $queryArray
+                );
+    $all_users = get_users( $args );
+    //To return users in CSV file
+    if ( $check == 1 ) {
+        $content = array();
+        foreach ( $all_users as $user ) {
+            $content_keys = array();
+            $metakey = array_unique( $meta_key );
+            foreach ( $metakey as $single_key ) {
+                $content_keys[] = get_user_meta( $user->ID, $single_key, true );
+            }
+            $content = array( $user->ID, $user->display_name, $user->user_email );
+            $finalcontent[] = array_merge( $content, $content_keys );
+        }
+        return $finalcontent;
+    }
+    //End
+    check_ajax_referer( 'bk-ajax-nonce', 'security' );
+    $resultSearch = array();
+    if ( $all_users ) {
+        foreach ( $all_users as $each_user ) {
+            $resultSearch[] = array( "id" => $each_user->ID, "username" => $each_user->display_name, "email" => $each_user->user_email );
+        }
+        wp_send_json( $resultSearch );
+    } else {
+        $resultSearch = -1;
+        wp_send_json( $resultSearch );
+    }
+    die;
+}
+
+add_action( 'wp_ajax_nopriv_append_meta', 'append_all_meta' );
+add_action( 'wp_ajax_append_meta', 'append_all_meta' );
+
+/**
+ * @author: PHPdev5
+ * @Description: Get Users'Role in Drop-Down Menu 
+ * @return [string]  
+ */
+function users_roles() {
+    check_ajax_referer( 'bk-ajax-nonce', 'security' );
+    global $wp_roles;
+    $role_data = '<option value="no_role">All</option>';
+    foreach ( $wp_roles->role_names as $role => $name ) :
+        $role_data .='<option value="' . $role . '">' . $role . '</option>';
+    endforeach;
+    echo $role_data;
+    die();
+}
+add_action( 'wp_ajax_nopriv_user-role', 'users_roles' );
+add_action( 'wp_ajax_user-role', 'users_roles' );
+
+/**
+ * @author: PHPdev5
+ * [setting_notes Add Notes for Users that used the plugin]
+ * @return [None]
+ */
+function setting_notes() {
+    if ( isset($_POST["save_notes"] ) ) {
+        $nonce = $_REQUEST['_wpnonce'];
+        if ( !wp_verify_nonce( $nonce, 'notes-users' ) ) {
+            die( 'Security check' );
+        }
+        $notes = $_POST["notes_users"];
+        update_option( 'notes-users', $notes );
+    }
+}
+
+add_action( "admin_init", 'setting_notes' );
+?>
